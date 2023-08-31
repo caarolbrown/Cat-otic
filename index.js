04c02558a62d5e33c47f05b67905062da1c78b94
Carol <carolinamorenofdez@gmail.com>
2023-08-31 16:26:43 +0100

    Cat added

---
+++ index.js
@@ -0,0 +1,22 @@
+//Declaración de variables
+
+const board = document.getElementById('main-board');
+const cat = new Cat(50, 750)
+
+//Función constructora gato:
+
+function Cat(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite
+    this.insertCat = function () {
+        var newCat = document.createElement('div');
+        newCat.setAttribute('id', 'cat');
+        newCat.style.left = this.x + 'px'
+        newCat.style.top = this.y + 'px'
+        board.appendChild(newCat)
+    }
+}
+
+cat.insertCat()
+

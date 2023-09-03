28c2a2a9b8ed266c92ff97fe7697d556ba31e10c
Carol Brown <carolbrown@Carols-MacBook-Air.local>
2023-09-03 11:11:01 +0100

    divs colocados correctamente

--- cat.js
+++ cat.js
@@ -1,14 +1,14 @@
-
-//Función constructora gato:
+//FunciÃ³n constructora gato:
 
 function Cat(x, y) {
     var self = this
     this.x = x;
     this.y = y;
     this.direction = 'none';
+    this.speed = 10;
     this.height = 70;
-    this.width = 100;
-    this.sprite = document.getElementById('cat');
+    this.width = 60;
+    this.sprite
 
     this.draw = function () {
         this.sprite.style.left = this.x + 'px'
@@ -18,76 +18,35 @@ function Cat(x, y) {
     this.move = function () {
         switch (this.direction) {
             case 'up':
-                if (this.y === 0) {
-                    this.direction = 'none'
+                if (this.y - this.speed <= 0) {
+                    this.direction = "none"
                 } else {
-                    this.y -= this.height
+                    this.y -= this.speed
                 }
                 break
             case 'left':
-                if (this.x === 0) {
-                    this.direction = 'none'
+                if (this.x - this.speed <= 0) {
+                    this.direction = "none"
                 } else {
-                    this.x -= this.width
+                    this.x -= this.speed
                 }
                 break
             case 'down':
-                if (this.y === 630) {
-                    this.direction = 'none'
+                if (this.y + this.speed >= 630) {
+                    this.direction = "none"
                 } else {
-                    this.y += this.height
+                    this.y += this.speed
                 }
                 break
             case 'right':
-                if (this.x === 1100) {
+                if (this.x + this.speed >= 1140) {
                     this.direction = 'none'
                 } else {
-                    this.x += this.width
+                    this.x += this.speed
                 }
                 break
         }
     }
-
-    this.wallsCollisions = function (walls) {
-console.log({offsetLeft: walls[0].offsetLeft, offsetTop: walls[0].offsetTop, offsetHeight: walls[0].offsetHeight, offsetWidth: walls[0].offsetWidth})
-        for (let i = 0; i < walls.length; i++) {
-            switch (this.direction) {
-                case 'right':
-                    if (
-                        this.x + this.width === walls[i].offsetLeft && this.y === walls[i].offsetTop
-            ) {
-                        return true
-                    }
-                    break;
-                case 'down':
-                    if (
-                        this.y + this.height === walls[i].offsetTop && this.x === walls[i].offsetLeft
-                    ) {
-                        return true
-                    }
-                    break;
-                case 'left':
-                    if (
-                        this.x - this.width === walls[i].offsetLeft && this.y === walls[i].offsetTop
-                    ) { 
-                        return true
-                    }
-                    break;
-                case 'up':
-                    if (
-                        this.y - this.height === walls[i].offsetTop && this.x === walls[i].offsetLeft
-                    ) { 
-                        //console.log({x: this.x, y: this.y, width: this.width, height: this.height, speed: this.speed, offsetLeft: walls[i].offsetLeft, offsetTop: walls[i].offsetTop, offsetHeight: walls[i].offsetHeight, offsetWidth: walls[i].offsetWidth})
-                        return true
-                     }
-                    break;
-                
-            }
-        } 
-        return false 
-    }
 }
 
-
-
-export { Cat } 
+export { Cat }
--- index.js
+++ index.js
@@ -1,73 +1,114 @@
 import { Cat } from './cat.js'
-import { Bullet } from './bullets.js';
+//import { Bullet } from './bullet.js'
 // import { Dogs } from './dogs.js'
 
-//Declaración de variables
+//DeclaraciÃ³n de variables
+function Game() {
+    this.self = this
+    this.board = document.getElementById('main-board')
+    this.walls = document.querySelectorAll('.walls')
+    this.cat = new Cat(30, 630)
+    this.bullets = []
 
-const board = document.getElementById('main-board');
-const walls = document.getElementsByClassName('walls')
+    this.insertCat = function () {
+        let newCat = document.createElement('div');
+        newCat.setAttribute('id', 'cat');
+        newCat.style.left = this.cat.x + "px"
+        newCat.style.top = this.cat.y + "px"
+        this.board.appendChild(newCat)
+        this.cat.sprite = document.getElementById('cat')
+    }
 
+    this.gameStart = function () {
+        this.insertCat()
+        var timerId = setInterval(this.loop, 20)
+    }
 
- //Deteccion de teclado:
+    this.wallsCollisions = function (player) {
+        for (let i = 0; i < this.walls.length; i++) {
+            switch (this.cat.direction) {
+                case "right":
+                    if (
+                        this.cat.x + this.cat.width + this.cat.speed >= this.walls[i].offsetLeft &&
+                        this.cat.x <= this.walls[i].offsetLeft + this.walls[i].offsetWidth &&
+                        this.cat.y + this.cat.height >= this.walls[i].offsetTop &&
+                        this.cat.y <= this.walls[i].offsetTop + this.walls[i].offsetHeight
+                    ) {
+                        console.log(this.walls[i].innerHTML)
+                        this.cat.direction = "none"
+                    };
+                    break;
+                case "down":
+                    if (
+                        this.cat.y + this.cat.height + this.cat.speed >= this.walls[i].offsetTop &&
+                        this.cat.y <= this.walls[i].offsetHeight + this.walls[i].offsetTop &&
+                        this.cat.x <= this.walls[i].offsetLeft + this.walls[i].offsetWidth &&
+                        this.cat.x + this.cat.width >= this.walls[i].offsetLeft
+                    ) {
+                        console.log(this.walls[i].innerHTML)
+                        this.cat.direction = "none"
+                    };
+                    break;
+                case "left":
+                    if (
+                        this.cat.x - this.cat.speed <= this.walls[i].offsetLeft + this.walls[i].offsetWidth &&
+                        this.cat.x + this.cat.width >= this.walls[i].offsetLeft &&
+                        this.cat.y + this.cat.height >= this.walls[i].offsetTop &&
+                        this.cat.y <= this.walls[i].offsetTop + this.walls[i].offsetHeight
+                    ) {
+                        console.log(this.walls[i].innerHTML)
+                        this.cat.direction = "none"
+                    };
+                    break;
+                case "up":
+                    if (
+                        this.cat.y - this.cat.speed <= this.walls[i].offsetTop + this.walls[i].offsetHeight &&
+                        this.cat.y + this.cat.height >= this.walls[i].offsetTop &&
+                        this.cat.x <= this.walls[i].offsetLeft + this.walls[i].offsetWidth &&
+                        this.cat.x + this.cat.width >= this.walls[i].offsetLeft
 
+                    ) {
+                        console.log(this.walls[i].innerHTML)
+                        this.cat.direction = "none"
+                    };
+                    break;
+            }
+        };
+    }
+
+    this.loop = function () {
+        game.wallsCollisions(this.cat)
+        game.wallsCollisions(this.dog)
+        if (game.cat.direction != "none") {
+            game.cat.move()
+            game.cat.draw()
+        }
+        game.cat.direction = "none"
+    }
+}
+
+var game = new Game()
+game.gameStart()
+
+//Deteccion de teclado:
 window.addEventListener('keydown', function (e) {
     switch (e.key) {
         case 'w':
-        cat.direction = 'up'
-        break
+            game.cat.direction = 'up'
+            break
         case 'a':
-        cat.direction = 'left'
-        break
+            game.cat.direction = 'left'
+            break
         case 's':
-        cat.direction = 'down'
-        break
+            game.cat.direction = 'down'
+            break
         case 'd':
-        cat.direction = 'right'
-        break
+            game.cat.direction = 'right'
+            break
         default:
-        cat.direction = "none"
-    }
-    //console.log(e.key)
-
-    if (cat.wallsCollisions(walls)){
-        cat.direction = 'none'
-    } else {
-        cat.move()
-        cat.draw()
+            game.cat.direction = "none"
     }
 })
-
-
-//Insertar al gato en pantalla
-
-var insertCat = function () {
-    var newCat = document.createElement('div');
-    newCat.setAttribute('id', 'cat');
-    board.appendChild(newCat)
-   
-}
-
-// Insert las balas en pantalla
-
-var insertBullet = function () {
-    var newBullet = document.createElement('div');
-    newBullet.setAttribute('id', 'bullet');
-    board.appendChild(newBullet)
-}
-
-
-
-insertCat()
-const cat = new Cat(0, 630)
-var timerId = setInterval(cat.move, 20);
-
-insertBullet()
-const bullet = new Bullet (1045, 560)
-
-cat.draw()
-bullet.draw()
-bullet.move()
-bullet.removeBullet()
 
 
 
